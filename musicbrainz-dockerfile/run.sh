51f160483fd538c0b14073675b72d10bb4e2c3fb
Jeff Sturgis <jeffsturgis@gmail.com>
2014-06-13 23:46:44 -0700

    fixed typo

--- musicbrainz-dockerfile/run.sh
+++ musicbrainz-dockerfile/run.sh
@@ -2,4 +2,4 @@
 DBDUMP=/media/jeff/storage/mbdata
 
 echo Starting musicbrainz...
-docker run --name musicbrainz -ti -p 5000:5000 --link postgresql:db -v $DBDUMP:/media/dbdump:rw musicbrainz-image /bin/bash
+docker run --name musicbrainz -d -p 5000:5000 --link postgresql:db -v $DBDUMP:/media/dbdump:rw musicbrainz-image
