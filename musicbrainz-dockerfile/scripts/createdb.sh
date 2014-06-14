aee581027da577fc321da05c568b44423934da55
Jeff Sturgis <jeffsturgis@gmail.com>
2014-06-14 01:42:06 -0700

    added script to get latest db dumps and import them also enabled replication

---
+++ musicbrainz-dockerfile/scripts/createdb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get install -y wget
+wget -nd -nH -P /media/dbdump ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST
+LATEST=$(cat /media/dbdump/LATEST)
+wget -r --no-parent -nd -nH -P /media/dbdump --reject "index.html*, mbdump-edit*, mbdump-documentation*" "ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST"
+pushd /media/dbdump && md5sum -c MD5SUMS && popd
+./admin/InitDb.pl --createdb --import /media/dbdump/mbdump*.tar.bz2 --echo
--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+CREATE_DB=$1
+if [ $CREATE_DB == true ]; then
+  /createdb.sh
+fi
+cron -f &
 redis-server --daemonize yes
 plackup -Ilib -r
