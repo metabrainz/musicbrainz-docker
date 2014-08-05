1e80721007faf7e9ac3118b6993bc0d40b5b9f6d
Jeff Sturgis <jeffsturgis@gmail.com>
2014-08-05 00:09:44 -0700

    remove old dumps first

--- musicbrainz-dockerfile/scripts/createdb.sh
+++ musicbrainz-dockerfile/scripts/createdb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get install -y wget
+rm -rf /media/dbdump/*
 wget -nd -nH -P /media/dbdump ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST
 LATEST=$(cat /media/dbdump/LATEST)
 wget -r --no-parent -nd -nH -P /media/dbdump --reject "index.html*, mbdump-edit*, mbdump-documentation*" "ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST"
