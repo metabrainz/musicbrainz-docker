5f6b9e46fd0bc6b2dd2751880b91fcabb26cc5dd
Jeff Sturgis <jeff.sturgis@retailnext.net>
2015-07-10 20:00:54 -0700

    Migrate to nginx
    Install nginx with apt.
    Add configuration file for nginx.
    Link to nginx config files in musicbrainz admin folder.
    Call nginx binary from start script.

--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -3,4 +3,5 @@
 env | grep '^DB_' | sed 's/^/export /' > /exports.txt
 cron -f &
 redis-server --daemonize yes
-plackup -Ilib -r
+nginx
+plackup -Ilib -s FCGI -E deployment --port 55901 --nproc 10 --pid fcgi.pid --keep-stderr=1
