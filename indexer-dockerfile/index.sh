ba49c57bbf57c7273020b62831e339d70a48f4dc
Robert Kaye <rob@musicbrainz.org>
2016-08-12 18:06:14 +0200

    Build all the indexes

--- indexer-dockerfile/index.sh
+++ indexer-dockerfile/index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/search
-java -jar /home/search/index.jar --indexes area --db-host db --db-name musicbrainz_db --db-user musicbrainz --db-password musicbrainz
+java -jar /home/search/index.jar --db-host db --db-name musicbrainz_db --db-user musicbrainz --db-password musicbrainz
