8f4b78ac933c69317fc330f301b01310d40d3912
Jeff Sturgis <jeffsturgis@gmail.com>
2016-09-20 15:29:48 -0700

    Update test-musicbrainz-image.sh
    Test that data was imported correctly.

--- test/test-musicbrainz-image.sh
+++ test/test-musicbrainz-image.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 sleep 2m
+PORT=5000
+HOST='localhost'
+PROTOCOL='http'
+HAS_SCRIPTS=$(curl "$PROTOCOL://$HOST:$PORT/" | grep '/static/build/common' 2> /dev/null)
+HAS_DATA=$(curl "$PROTOCOL://$HOST:$PORT/artist/af8e4cc5-ef54-458d-a194-7b210acf638f" | grep 'Cannibal Corpse' 2> /dev/null)
 
-HAS_SCRIPTS=$(curl 'http://localhost:5000/' | grep '/static/build/common' 2> /dev/null)
+if [ "$HAS_SCRIPTS" != '' ]; then
+  echo 'Musicbrainz built correctly'
+else
+  echo 'Musicbrainz did not build correctly'
+fi
 
-if [[ -n $HAS_SCRIPTS ]]; then
+if [ "$HAS_DATA" != '' ]; then
+  echo 'Musicbrainz DB was created and data was imported correctly'
+else
+  echo 'Musicbrainz DB was not created or data was not imported correctly'
+fi
+
+if [ "$HAS_SCRIPTS" != '' -a "$HAS_DATA" != '' ]; then
   exit 0
 fi
 
