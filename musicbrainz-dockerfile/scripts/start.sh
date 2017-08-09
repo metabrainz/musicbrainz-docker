a56af827922d21550c4d2581bfcc84b8fb5c07f0
Robert Kaye <rob@musicbrainz.org>
2017-08-09 12:05:02 +0200

    Use separate images for nginx, redis. Use dockerizer to wait for dependent containers to start.

--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -5,7 +5,5 @@ eval $( perl -Mlocal::lib )
 env | grep '^DB_' | sed 's/^/export /' > /exports.txt
 
 cron -f &
-redis-server --daemonize yes
-nginx
 /start_mb_renderer.pl
 start_server --port=55901 -- plackup -I lib -s Starlet -E deployment --nproc 10 --pid fcgi.pid
