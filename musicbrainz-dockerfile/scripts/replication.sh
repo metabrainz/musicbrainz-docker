ac4ff3302455bde7415a3fb9361f0eecf7fd4509
Jeff Sturgis <jeffsturgis@gmail.com>
2017-08-23 22:27:22 -0700

    Fix replication cron job errors
    Add the environment variables required to run the replication cron job.

--- musicbrainz-dockerfile/scripts/replication.sh
+++ musicbrainz-dockerfile/scripts/replication.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 eval $( /usr/bin/perl -Mlocal::lib )
-. /exports.txt
+export POSTGRES_USER=musicbrainz
+export POSTGRES_PASSWORD=musicbrainz
+
 /bin/bash /musicbrainz-server/admin/cron/slave.sh
