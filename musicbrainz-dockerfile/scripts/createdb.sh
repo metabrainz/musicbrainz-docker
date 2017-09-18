e2d70b95c909f6767a1c8441d3326ef1065b57c0
Robert Kaye <rob@musicbrainz.org>
2017-09-18 18:25:26 +0200

    Use the faster EU mirror and make the TMP_DIR before InitDB.pl

--- musicbrainz-dockerfile/scripts/createdb.sh
+++ musicbrainz-dockerfile/scripts/createdb.sh
@@ -32,6 +32,7 @@ fi
 if [[ -a /media/dbdump/mbdump.tar.bz2 ]]; then
   echo "found existing dumps"
 
+  mkdir -p $TMP_DIR
   /musicbrainz-server/admin/InitDb.pl --import /media/dbdump/mbdump*.tar.bz2 --tmp-dir $TMP_DIR --echo
 else
   echo "no dumps found or dumps are incomplete"
