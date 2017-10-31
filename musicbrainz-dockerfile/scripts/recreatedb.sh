193b0c7b227863c32bcfab58180492cef72b7b79
Jeff Sturgis <jeffsturgis@gmail.com>
2017-10-31 13:57:47 -0700

    Update recreatedb.sh
    Replace the user constant with the correct value.

--- musicbrainz-dockerfile/scripts/recreatedb.sh
+++ musicbrainz-dockerfile/scripts/recreatedb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql -U $DB_ENV_POSTGRES_USER -h db -c "DROP DATABASE musicbrainz;" postgres && /createdb.sh -fetch
+psql -U musicbrainz -h db -c "DROP DATABASE musicbrainz;" postgres && /createdb.sh -fetch
