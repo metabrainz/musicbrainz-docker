8a6f41c9ec5ef0a824060fc8f6ef06e8474b6614
Jeff Sturgis <jesturgis@linkedin.com>
2017-11-01 23:38:01 -0700

    upgrade to v-2017-10-31

--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -3,6 +3,5 @@
 eval $( perl -Mlocal::lib )
 
 cron -f &
-nginx
 /start_mb_renderer.pl
-start_server --port=55901 -- plackup -I lib -s Starlet -E deployment --nproc 10 --pid fcgi.pid
+start_server --port=5000 -- plackup -I lib -s Starlet -E deployment --nproc 10 --pid fcgi.pid
