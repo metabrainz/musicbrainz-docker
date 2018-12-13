9a79cc5ff364d64a27e536910b5fef5fd2d014e2
yvanzo <yvanzo@metabrainz.org>
2018-12-13 13:50:11 +0100

    Fix indentation

--- musicbrainz-dockerfile/scripts/createdb.sh
+++ musicbrainz-dockerfile/scripts/createdb.sh
@@ -11,31 +11,31 @@ if [[ $2 != "" ]]; then
 fi
 
 if [[ $FETCH_DUMPS == "-fetch" ]]; then
-  echo "fetching data dumps"
+    echo "fetching data dumps"
 
-  apt-get install -y wget
-  rm -rf /media/dbdump/*
-  wget -nd -nH -P /media/dbdump $FTP_MB/data/fullexport/LATEST
-  LATEST=$(cat /media/dbdump/LATEST)
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/MD5SUMS"
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump.tar.bz2"
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-cdstubs.tar.bz2"
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-cover-art-archive.tar.bz2"
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-derived.tar.bz2"
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-stats.tar.bz2"
-  wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-wikidocs.tar.bz2"
-  pushd /media/dbdump && md5sum -c MD5SUMS && popd
+    apt-get install -y wget
+    rm -rf /media/dbdump/*
+    wget -nd -nH -P /media/dbdump $FTP_MB/data/fullexport/LATEST
+    LATEST=$(cat /media/dbdump/LATEST)
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/MD5SUMS"
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump.tar.bz2"
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-cdstubs.tar.bz2"
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-cover-art-archive.tar.bz2"
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-derived.tar.bz2"
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-stats.tar.bz2"
+    wget -P /media/dbdump "$FTP_MB/data/fullexport/$LATEST/mbdump-wikidocs.tar.bz2"
+    pushd /media/dbdump && md5sum -c MD5SUMS && popd
 fi
 
 if [[ -a /media/dbdump/mbdump.tar.bz2 ]]; then
-  echo "found existing dumps"
+    echo "found existing dumps"
 
-  mkdir -p $TMP_DIR
+    mkdir -p $TMP_DIR
 
-  # if the import fails because the DB does not exist yet such as when the DB
-  # has been dropped, InitDb will be called again with the create flag
-  /musicbrainz-server/admin/InitDb.pl --echo --import -- --skip-editor --tmp-dir $TMP_DIR /media/dbdump/mbdump*.tar.bz2 ||
-  /musicbrainz-server/admin/InitDb.pl --create --echo --import -- --skip-editor --tmp-dir $TMP_DIR /media/dbdump/mbdump*.tar.bz2
+    # if the import fails because the DB does not exist yet such as when the DB
+    # has been dropped, InitDb will be called again with the create flag
+    /musicbrainz-server/admin/InitDb.pl --echo --import -- --skip-editor --tmp-dir $TMP_DIR /media/dbdump/mbdump*.tar.bz2 ||
+    /musicbrainz-server/admin/InitDb.pl --create --echo --import -- --skip-editor --tmp-dir $TMP_DIR /media/dbdump/mbdump*.tar.bz2
 else
-  echo "no dumps found or dumps are incomplete"
+    echo "no dumps found or dumps are incomplete"
 fi
