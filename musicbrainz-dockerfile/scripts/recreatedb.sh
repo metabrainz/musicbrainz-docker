5bc7fd1f10936a484bfd4be435edd6711d876a9c
yvanzo <yvanzo@metabrainz.org>
2018-12-13 14:41:38 +0100

    Add '-sample' option to create db for sample data
    additionally document script usage and catch usage errors.

--- musicbrainz-dockerfile/scripts/recreatedb.sh
+++ musicbrainz-dockerfile/scripts/recreatedb.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 eval $( perl -Mlocal::lib )
 
-FETCH_DUMPS=$1
-
-psql postgres -U musicbrainz -h db -c "DROP DATABASE musicbrainz_db;"; /createdb.sh $FETCH_DUMPS
+psql postgres -U musicbrainz -h db -c "DROP DATABASE musicbrainz_db;"; /createdb.sh "$@"
