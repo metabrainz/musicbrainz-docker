b00d8dde34df8ba511151476dccde9232ac2217d
yvanzo <yvanzo@metabrainz.org>
2018-12-26 18:40:02 +0100

    Build and preload AMQP extension for PostgreSQL
    which is required to trigger Search Index Rebuilder (SIR).

--- postgres-dockerfile/set-config.sh
+++ postgres-dockerfile/set-config.sh
@@ -2,5 +2,6 @@
 
 echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf
 echo "shared_buffers = 512MB" >> /var/lib/postgresql/data/postgresql.conf
+echo "shared_preload_libraries = 'pg_amqp.so'" >> /var/lib/postgresql/data/postgresql.conf
 
 exec /docker-entrypoint.sh 
