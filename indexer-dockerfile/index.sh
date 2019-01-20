63f568535104227ee887f40ac588fc2065add274
yvanzo <yvanzo@metabrainz.org>
2019-01-20 12:13:17 +0100

    Replace old indexer with sir for 'indexer' service
    - It depends on search service instead of shared volume 'indexdata'
    
    - Helper script index.js has been removed, instead one can run:
    
      python -m sir reindex

--- indexer-dockerfile/index.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/search
-java -jar /home/search/index.jar --db-host db --db-name musicbrainz_db --db-user musicbrainz --db-password musicbrainz --indexes-dir /home/search/indexdata
