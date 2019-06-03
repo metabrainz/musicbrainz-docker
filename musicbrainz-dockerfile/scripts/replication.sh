a7b0a2fd3347a6d6b9f5ddbeb27232065cd7b220
yvanzo <yvanzo@metabrainz.org>
2019-06-03 17:13:52 +0200

    Fix replication.sh to use environment POSTGRES_*
    `replication.sh` was overriding it with `musicbrainz`/`musicbrainz`.
    This script now uses these values as defaults not as overrides.

--- musicbrainz-dockerfile/scripts/replication.sh
+++ musicbrainz-dockerfile/scripts/replication.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 eval $( /usr/bin/perl -Mlocal::lib )
-export POSTGRES_USER=musicbrainz
-export POSTGRES_PASSWORD=musicbrainz
+export POSTGRES_USER=${POSTGRES_USER:-musicbrainz}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-musicbrainz}
 
 /bin/bash /musicbrainz-server/admin/cron/slave.sh
