571d595769ccf05f1203fd7d5892086d958ec4e1
yvanzo <yvanzo@metabrainz.org>
2019-06-22 17:39:42 +0200

    Allow setting web server host/port via environment
    Service 'musicbrainz' has two new Docker environment variables:
    - MUSICBRAINZ_WEB_SERVER_HOST (default: "localhost")
    - MUSICBRAINZ_WEB_SERVER_PORT (default: "5000")
    
    If these have changed of value, static resources are recompiled.

--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -2,6 +2,13 @@
 
 eval $( perl -Mlocal::lib )
 
+if ! grep -q -s \
+  "//${MUSICBRAINZ_WEB_SERVER_HOST}:${MUSICBRAINZ_WEB_SERVER_PORT}" \
+  /musicbrainz-server/root/static/build/runtime.js.map
+then
+  /musicbrainz-server/script/compile_resources.sh
+fi
+
 cron -f &
 /start_mb_renderer.pl
 start_server --port=5000 -- plackup -I lib -s Starlet -E deployment --nproc 10 --pid fcgi.pid
