22d2d1bcfac0b660cabfb06143bf5b149021b3fa
yvanzo <yvanzo@metabrainz.org>
2019-12-02 15:18:09 +0100

    Also assume 'wheel' group members are sudoers
    Previously, only 'sudo' group members were assumed to be sudoers,
    which is usually correct for Debian and derivative.
    
    This patch additionally assumes 'wheel' group members are sudoers,
    which is usually correct for other Linux distributions.
    
    It improves sudo detection for setting docker(-compose) commands.

--- admin/lib/common.inc.bash
+++ admin/lib/common.inc.bash
@@ -6,7 +6,7 @@ cd "$MB_DOCKER_ROOT"
 
 if [ -z ${DOCKER_CMD:+smt} ]
 then
-  if groups | grep -qw sudo
+  if groups | grep -Pqw 'sudo|wheel'
   then
     DOCKER_CMD='sudo docker'
   elif groups | grep -Pqw 'docker|root'
@@ -14,7 +14,7 @@ then
     DOCKER_CMD='docker'
   else
     echo >&2 "$SCRIPT_NAME: cannot set docker command: please either"
-    echo >&2 "  * add the user '$USER' to the group 'sudo'"
+    echo >&2 "  * add the user '$USER' to the group 'sudo' or 'wheel'"
     echo >&2 "  * or set the variable \$DOCKER_CMD"
     exit 77
   fi
@@ -22,16 +22,16 @@ fi
 
 if [ -z ${DOCKER_COMPOSE_CMD:+smt} ]
 then
-  if groups | grep -qw sudo
+  if groups | grep -Pqw 'sudo|wheel'
   then
     DOCKER_COMPOSE_CMD='sudo docker-compose'
   elif groups | grep -Pqw 'docker|root'
   then
     DOCKER_COMPOSE_CMD='docker-compose'
   else
-    echo >&2 "$SCRIPT_NAME: cannot set docker-compose command: please try"
-    echo >&2 "  * either adding the user '$USER' to the group 'sudo',"
-    echo >&2 "  * or setting the variable \$DOCKER_COMPOSE_CMD."
+    echo >&2 "$SCRIPT_NAME: cannot set docker-compose command: please either"
+    echo >&2 "  * add the user '$USER' to the group 'sudo' or 'wheel'"
+    echo >&2 "  * or set the variable \$DOCKER_COMPOSE_CMD"
     exit 77
   fi
 fi
