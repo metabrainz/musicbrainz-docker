2400db14a648b4fff0e2fe1ac884fe5123e05123
yvanzo <yvanzo@metabrainz.org>
2020-04-12 09:58:30 +0200

    Install Node modules without waiting for services

--- build/musicbrainz-dev/scripts/start.sh
+++ build/musicbrainz-dev/scripts/start.sh
@@ -30,6 +30,8 @@ cpanm --notest \
   Starlet::Server \
   Term::Size::Any
 
+yarn
+
 dockerize -wait tcp://db:5432 -timeout 60s -wait tcp://mq:5672 -timeout 60s -wait tcp://redis:6379 -timeout 60s ./script/compile_resources.sh
 
 /start_mb_renderer.pl
