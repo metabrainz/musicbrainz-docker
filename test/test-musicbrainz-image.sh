434e6aaa013d8a4a19d8dcf31bdc2afff412fb3f
yvanzo <yvanzo@metabrainz.org>
2020-04-20 20:27:47 +0200

    Update and rework script to test musicbrainz image
    * Update to the current location of CSS built file
    * Rework to provide more details on test failure

--- test/test-musicbrainz-image.sh
+++ test/test-musicbrainz-image.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-set -e -u
+set -e -o pipefail -u
 
 sleep 2m
 
-if curl --retry 20 --retry-delay 20 'http://localhost:5000/' \
-  | grep -q '/static/build/styles/common'
+echo -n 'Test local website has locally built style sheet... '
+if curl -sS --retry 20 --retry-delay 20 'http://localhost:5000/' \
+  | grep -q '/static/build/common-[0-9a-f]*.css'
 then
-  echo 'Local website has locally built scripts'
-  exit 0
+  echo OK
+  exit 0 # EX_OK
+else
+  echo FAIL
+  echo "'curl|grep' exited with status '$?'"
+  echo "Content of 'http://localhost:5000/':"
+  curl -sS --retry 20 --retry-delay 20 'http://localhost:5000/'
+  exit 80 # EX_CSSNOTFOUND
 fi
-
-exit 1
