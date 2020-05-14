c76047814e923dd68ad5f7512586ec38fa44fd87
yvanzo <yvanzo@metabrainz.org>
2020-05-14 15:40:50 +0200

    Set Postgres configuration parameters from command
    The two additional configuration parameters were hardcoded in a script
    installed in the Docker image for the service 'db'. It was not easy to
    change 'shared_buffers' value to meet meet resources and satisfy needs.
    
    This patch drops the script and set configuration parameters from the
    'command' defined for the db service. It allows to change these values
    without having to rebuild images, and to add any other configuration
    parameter or option: just override 'command' in a local compose file.
    
    It also increases the value for shared_buffers to 2048MB by default,
    which is more suitable when running a mirror to speed web service up.
    
    References:
    * https://hub.docker.com/_/postgres/
    * https://www.postgresql.org/docs/9.5/app-postgres.html

--- build/postgres/set-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf
-echo "shared_buffers = 512MB" >> /var/lib/postgresql/data/postgresql.conf
-echo "shared_preload_libraries = 'pg_amqp.so'" >> /var/lib/postgresql/data/postgresql.conf
-
-exec /docker-entrypoint.sh 
