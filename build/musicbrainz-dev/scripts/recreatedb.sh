a17536e775514b5dae4084dd603d4be7dec3ed60
yvanzo <yvanzo@metabrainz.org>
2020-05-15 13:11:42 +0200

    Close this branch after v-2020-05-14-solr7 release

--- build/musicbrainz-dev/scripts/recreatedb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-eval "$(perl -Mlocal::lib="${MUSICBRAINZ_PERL_LOCAL_LIB}")"
-
-psql postgres -U musicbrainz -h db -c "DROP DATABASE musicbrainz_db;"; createdb.sh "$@"
--- build/musicbrainz-dev/scripts/snippet.perllocallib.bashrc
+++
@@ -1,5 +0,0 @@
-
-# Enable locally installed Perl modules for interactive shell
-if [[ $- = *i* ]]; then
-    eval "$(perl -Mlocal::lib="${MUSICBRAINZ_PERL_LOCAL_LIB}")"
-fi
