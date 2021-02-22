b0a07c4ece50a58cf9e7bf8242b3295de49e6f7c
yvanzo <yvanzo@metabrainz.org>
2021-02-22 13:01:16 +0100

    Split JS deps/scripts update off from start.sh
    It allows to easily update JavaScript dependencies and scripts from
    within the container.

--- build/musicbrainz-dev/scripts/start.sh
+++ build/musicbrainz-dev/scripts/start.sh
@@ -6,9 +6,7 @@ cd /musicbrainz-server
 
 update-perl.sh
 
-yarn
-
-dockerize -wait tcp://db:5432 -timeout 60s -wait tcp://mq:5672 -timeout 60s -wait tcp://redis:6379 -timeout 60s ./script/compile_resources.sh
+update-javascript.sh
 
 start_mb_renderer.pl
 start_server --port=5000 -- plackup -I lib -s Starlet -E deployment --nproc ${MUSICBRAINZ_SERVER_PROCESSES} --pid fcgi.pid
---
+++ build/musicbrainz-dev/scripts/update-javascript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -u
+
+cd /musicbrainz-server
+
+yarn
+
+dockerize -wait tcp://db:5432 -timeout 60s -wait tcp://mq:5672 -timeout 60s -wait tcp://redis:6379 -timeout 60s ./script/compile_resources.sh
+
