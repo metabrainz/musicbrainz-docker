c70819f7857ee1bd58b78e3e27fbfb5bba733bfb
yvanzo <yvanzo@metabrainz.org>
2021-02-22 12:51:05 +0100

    Split Perl config/deps update off from start.sh
    It allows to easily update Perl configuration and dependencies from
    within the container, for example to run `createdb.sh` (MBVM-66).

--- build/musicbrainz-dev/scripts/start.sh
+++ build/musicbrainz-dev/scripts/start.sh
@@ -4,24 +4,7 @@ set -e -u
 
 cd /musicbrainz-server
 
-diff /DBDefs.pm lib/DBDefs.pm || cat /DBDefs.pm > lib/DBDefs.pm
-
-cpanm --installdeps --notest --with-develop .
-cpanm --notest \
-  Cache::Memcached::Fast \
-  Cache::Memory \
-  Catalyst::Plugin::Cache::HTTP \
-  Catalyst::Plugin::StackTrace \
-  Digest::MD5::File \
-  File::Slurp \
-  JSON::Any \
-  LWP::Protocol::https \
-  Plack::Handler::Starlet \
-  Plack::Middleware::Debug::Base \
-  Server::Starter \
-  Starlet \
-  Starlet::Server \
-  Term::Size::Any
+update-perl.sh
 
 yarn
 
