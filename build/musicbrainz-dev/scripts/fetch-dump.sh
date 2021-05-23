metabrainz/musicbrainz-docker

commit 63c96d0be7ab00e967ed84b9a211c7693969ef58

MBVM-70: Allow setting FTP mirror through `.env`
Allow setting FTP mirror URL through the Docker environment variable
`MUSICBRAINZ_BASE_FTP_URL`.

Default value is still `ftp://ftp.eu.metabrainz.org/pub/musicbrainz`.

build/musicbrainz-dev/scripts/fetch-dump.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 DB_DUMP_DIR=/media/dbdump
 SEARCH_DUMP_DIR=/media/searchdump
-BASE_FTP_URL='ftp://ftp.eu.metabrainz.org/pub/musicbrainz'
+BASE_FTP_MB="$MUSICBRAINZ_BASE_FTP_URL"
 TARGET=''
 WGET_CMD=(wget)
 

build/musicbrainz/scripts/fetch-dump.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 DB_DUMP_DIR=/media/dbdump
 SEARCH_DUMP_DIR=/media/searchdump
-BASE_FTP_URL='ftp://ftp.eu.metabrainz.org/pub/musicbrainz'
+BASE_FTP_URL="$MUSICBRAINZ_BASE_FTP_URL"
 TARGET=''
 WGET_CMD=(wget)
 

build/musicbrainz-dev/Dockerfile
@@ -103,6 +103,7 @@ ARG POSTGRES_USER=doesntmatteraslongasyoudontcompiletests
 ARG POSTGRES_PASSWORD=doesntmatteraslongasyoudontcompiletests
 
 ENV BASH_ENV=/noninteractive.bash_env \
+    MUSICBRAINZ_BASE_FTP_URL=ftp://ftp.eu.metabrainz.org/pub/musicbrainz \
     MUSICBRAINZ_CATALYST_DEBUG=0 \
     MUSICBRAINZ_DEVELOPMENT_SERVER=1 \
     MUSICBRAINZ_SEARCH_SERVER=search:8983/solr \

build/musicbrainz-dev/scripts/createdb.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail -u
 
-FTP_MB=ftp://ftp.eu.metabrainz.org/pub/musicbrainz
+FTP_MB="$MUSICBRAINZ_BASE_FTP_URL"
 IMPORT="fullexport"
 FETCH_DUMPS=""
 WGET_OPTIONS=""

build/musicbrainz/Dockerfile
@@ -121,6 +121,7 @@ ARG POSTGRES_USER=doesntmatteraslongasyoudontcompiletests
 ARG POSTGRES_PASSWORD=doesntmatteraslongasyoudontcompiletests
 
 ENV BASH_ENV=/noninteractive.bash_env \
+    MUSICBRAINZ_BASE_FTP_URL=ftp://ftp.eu.metabrainz.org/pub/musicbrainz \
     MUSICBRAINZ_CATALYST_DEBUG=0 \
     MUSICBRAINZ_DEVELOPMENT_SERVER=0 \
     MUSICBRAINZ_SEARCH_SERVER=search:8983/solr \

build/musicbrainz/scripts/createdb.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail -u
 
-FTP_MB=ftp://ftp.eu.metabrainz.org/pub/musicbrainz
+FTP_MB="$MUSICBRAINZ_BASE_FTP_URL"
 IMPORT="fullexport"
 FETCH_DUMPS=""
 WGET_OPTIONS=""

docker-compose.yml
@@ -55,6 +55,7 @@ services:
     env_file:
       - ./default/postgres.env
     environment:
+      - MUSICBRAINZ_BASE_FTP_URL=${MUSICBRAINZ_BASE_FTP_URL:-ftp://ftp.eu.metabrainz.org/pub/musicbrainz}
       - MUSICBRAINZ_SERVER_PROCESSES=${MUSICBRAINZ_SERVER_PROCESSES:-10}
       - MUSICBRAINZ_USE_PROXY=1
       - MUSICBRAINZ_WEB_SERVER_HOST=${MUSICBRAINZ_WEB_SERVER_HOST:-localhost}

commit 81fa40453ce400c14580bb0259b7b117be5e6318

Fix fetch-dump.sh script in development setup (#197)
This fixes a regression caused by commit 63c96d0be7ab00e967ed84b9a211c7693969ef58:

    $ sudo docker-compose run --rm musicbrainz createdb.sh -sample -fetch
    /usr/local/bin/fetch-dump.sh: line 33: BASE_FTP_URL: unbound variable

build/musicbrainz-dev/scripts/fetch-dump.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -u
 
 DB_DUMP_DIR=/media/dbdump
 SEARCH_DUMP_DIR=/media/searchdump
-BASE_FTP_MB="$MUSICBRAINZ_BASE_FTP_URL"
+BASE_FTP_URL="$MUSICBRAINZ_BASE_FTP_URL"
 TARGET=''
 WGET_CMD=(wget)
 