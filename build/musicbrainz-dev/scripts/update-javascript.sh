9ac2a4bcd43eceafb047c4e3bed85658e4834567
yvanzo <yvanzo@metabrainz.org>
2021-09-20 10:44:55 +0200

    Compile JavaScript tests in watch mode too

--- build/musicbrainz-dev/scripts/update-javascript.sh
+++ build/musicbrainz-dev/scripts/update-javascript.sh
@@ -8,4 +8,4 @@ yarn
 
 dockerize -wait tcp://db:5432 -timeout 60s -wait tcp://mq:5672 -timeout 60s -wait tcp://redis:6379 -timeout 60s
 
-./script/compile_resources.sh --watch &> /compile_resources.log &
+./script/compile_resources.sh --watch client server tests &> /compile_resources.log &
