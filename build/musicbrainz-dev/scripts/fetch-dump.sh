2d9eca751fd9cc6c62db56cc7be8b60471056e31
yvanzo <yvanzo@metabrainz.org>
2023-04-17 20:17:28 +0100

    Amend bb25a88ec0a98990c1450a717d420ce65440dd98

--- build/musicbrainz-dev/scripts/fetch-dump.sh
+++ build/musicbrainz-dev/scripts/fetch-dump.sh
@@ -51,7 +51,7 @@ do
 			;;
 		--base-download-url )
 			shift
-			BASE_DOWNLOAD_URL="${1%/base/fullexport/}"
+			BASE_DOWNLOAD_URL="${1%/data/fullexport/}"
 			if ! [[ $BASE_DOWNLOAD_URL =~ ^(ftp|https?):// ]]
 			then
 				echo >&2 "$SCRIPT_NAME: --base-download-url must begin with ftp://, http:// or https://"
