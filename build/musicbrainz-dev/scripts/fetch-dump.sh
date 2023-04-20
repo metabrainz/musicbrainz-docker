0236211d54d4545faf98e7af723970be507627d7
yvanzo <yvanzo@metabrainz.org>
2023-04-20 16:41:34 +0100

    Refactoring: Set base download URL once for all
    Reminder: The container environment variable `MUSICBRAINZ_BASE_FTP_URL`
    has precedence for backward compatibility reasons. See the message for
    commit fddeeaa262c0481fed9ab1bf71e0e7073c5291be which is amended here.

--- build/musicbrainz-dev/scripts/fetch-dump.sh
+++ build/musicbrainz-dev/scripts/fetch-dump.sh
@@ -98,6 +98,10 @@ then
 	exit 64 # EX_USAGE
 fi
 
+# Keep support for (deprecated) FTP option (which still takes precedence)
+
+BASE_DOWNLOAD_URL="${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}"
+
 # Fetch latest search indexes
 
 if [[ $TARGET =~ ^(both|search)$ ]]
@@ -105,10 +109,10 @@ then
 	echo "$(date): Fetching search indexes dump..."
 	cd "$SEARCH_DUMP_DIR" && find . -delete && cd -
 	"${WGET_CMD[@]}" -nd -nH -P "$SEARCH_DUMP_DIR" \
-		"${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}/data/search-indexes/LATEST"
+		"${BASE_DOWNLOAD_URL}/data/search-indexes/LATEST"
 	DUMP_TIMESTAMP=$(cat /media/searchdump/LATEST)
 	"${WGET_CMD[@]}" -nd -nH -r -P "$SEARCH_DUMP_DIR" \
-		"${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}/data/search-indexes/$DUMP_TIMESTAMP/"
+		"${BASE_DOWNLOAD_URL}/data/search-indexes/$DUMP_TIMESTAMP/"
 	cd "$SEARCH_DUMP_DIR" && md5sum -c MD5SUMS && cd -
 	if [[ $TARGET == search ]]
 	then
@@ -152,7 +156,7 @@ then
 
 	SEARCH_DUMP_DAY="${DUMP_TIMESTAMP/-*}"
 	"${WGET_CMD[@]}" --spider --no-remove-listing -P "$DB_DUMP_DIR" \
-		"${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR"
+		"${BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR"
 	DUMP_TIMESTAMP=$(
 		grep -E "\\s${SEARCH_DUMP_DAY}-\\d*" "$DB_DUMP_DIR/.listing" \
 			| sed -e 's/\s*$//' -e 's/.*\s//'
@@ -164,7 +168,7 @@ then
 	# Just find latest database dump
 
 	"${WGET_CMD[@]}" -nd -nH -P "$DB_DUMP_DIR" \
-		"${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR/LATEST"
+		"${BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR/LATEST"
 	DUMP_TIMESTAMP=$(cat "$DB_DUMP_DIR/LATEST")
 fi
 
@@ -175,7 +179,7 @@ then
 	for F in MD5SUMS "${DB_DUMP_FILES[@]}"
 	do
 		"${WGET_CMD[@]}" -P "$DB_DUMP_DIR" \
-			"${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR/$DUMP_TIMESTAMP/$F"
+			"${BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR/$DUMP_TIMESTAMP/$F"
 	done
 	cd "$DB_DUMP_DIR"
 	for F in "${DB_DUMP_FILES[@]}"
@@ -192,7 +196,7 @@ then
 	for F in "${DB_DUMP_FILES[@]}"
 	do
 		"${WGET_CMD[@]}" -P "$DB_DUMP_DIR" \
-			"${BASE_FTP_URL:-$BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR/$DUMP_TIMESTAMP/$F"
+			"${BASE_DOWNLOAD_URL}/$DB_DUMP_REMOTE_DIR/$DUMP_TIMESTAMP/$F"
 	done
 fi
 
