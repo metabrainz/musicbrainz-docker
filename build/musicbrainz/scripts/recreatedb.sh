2317ebf792455c9841223384cf69fedbdd26b99b
yvanzo <yvanzo@metabrainz.org>
2023-05-22 12:26:38 +0100

    Avoid stopping if the database doesn’t exist
    The script `recreatedb.sh` was stopping when the database didn’t exist,
    while the expectation is to keep creating the database even then.
    
    This patch prevents triggering an error when the database doesn’t exist.
    
    Submitted by @PeterCodar in a comment.

--- build/musicbrainz/scripts/recreatedb.sh
+++ build/musicbrainz/scripts/recreatedb.sh
@@ -3,4 +3,4 @@
 set -e
 
 dockerize -wait tcp://db:5432 -timeout 60s sleep 0
-psql postgres -U musicbrainz -h db -c "DROP DATABASE musicbrainz_db;"; createdb.sh "$@"
+psql postgres -U musicbrainz -h db -c "DROP DATABASE IF EXISTS musicbrainz_db;"; createdb.sh "$@"
