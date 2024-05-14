371e419811fbc6a732ea982ec5c9063326a76580
yvanzo <yvanzo@users.noreply.github.com>
2024-05-14 16:16:35 +0100

    Upgrade Server to v-2024-05-13-schema-change (#278)
    It brings MusicBrainz database schema 29 and requires manual upgrade.
    
    See release notes for detailed upgrade steps.

--- build/musicbrainz/scripts/upgrade-db-schema.sh
+++ build/musicbrainz/scripts/upgrade-db-schema.sh
@@ -2,6 +2,6 @@
 
 set -e -u
 
-export MUSICBRAINZ_DB_SCHEMA_SEQUENCE=27
+export MUSICBRAINZ_DB_SCHEMA_SEQUENCE=28
 
 dockerize -wait "tcp://${MUSICBRAINZ_POSTGRES_SERVER}:5432" -timeout 60s carton exec -- ./upgrade.sh
