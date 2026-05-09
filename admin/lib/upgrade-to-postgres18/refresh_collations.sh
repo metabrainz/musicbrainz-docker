cb7a0a620ba7ebc02c2c0f6a6145c206cab3b14b
Michael Wiencek <mwtuea@gmail.com>
2026-05-09 17:26:54 -0500

    Remove full cluster reindexing from admin/upgrade-to-postgres18
    Only a limited subset of indexes in musicbrainz_db need to be rebuilt. None in
    the 'postgres' or 'template1' databases are affected. We can document calling
    the RebuildIndexesUsingCollations.pl script from musicbrainz-server in the
    upgrade notes.

---
+++ admin/lib/upgrade-to-postgres18/refresh_collations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Refreshes the collation versions for all databases.
+
+DATABASES=$(
+    psql \
+        -U musicbrainz \
+        -d template1 \
+        -c 'SELECT datname FROM pg_database WHERE datallowconn' \
+        -Atq
+)
+
+for database in $DATABASES; do
+    OUTPUT="$(psql -U musicbrainz -d "$database" -c "ALTER DATABASE $database REFRESH COLLATION VERSION;" 2>&1)" \
+        || { printf '%s\n' "$OUTPUT" >&2; exit 1; }
+done
